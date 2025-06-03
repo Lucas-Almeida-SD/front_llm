4592aa4bd0ae9d2339f7c65779d9e1215397f0b0
Lucas <lucas.almdiniz@gmail.com>
2025-06-02 22:06:55 -0400

    feat: Criado componente Form

---
+++ src/components/molecules/Form/index.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/atoms/Button";
+import Textarea from "@/components/atoms/Textarea";
+
+export default function Form() {
+  return(
+    <form>
+      <Textarea />
+      <Button title="Enviar" />
+    </form>
+  );
+}
