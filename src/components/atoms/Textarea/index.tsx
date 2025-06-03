e220676446f23a383f48998020b0b589d6e063a1
Lucas <lucas.almdiniz@gmail.com>
2025-06-02 22:06:05 -0400

    feat: Criado componente Textarea

---
+++ src/components/atoms/Textarea/index.tsx
@@ -0,0 +1,7 @@
+export default function Textarea() {
+  return(
+    <div className="mb-3">
+      <textarea className="form-control" id="question-input" rows={3} placeholder="Como posso ajudar?" />
+    </div>
+  );
+}
