ecb559e7777b90e49e8404673e9932ccbb7988e7
Lucas <lucas.almdiniz@gmail.com>
2025-06-07 00:24:17 -0400

    refactor: Adicionado titulo para o formulario

--- src/components/molecules/Form/index.tsx
+++ src/components/molecules/Form/index.tsx
@@ -1,11 +1,16 @@
 import Button from "@/components/atoms/Button";
 import Textarea from "@/components/atoms/Textarea";
 
+import styles from './styles.module.scss';
+
 export default function Form() {
   return(
-    <form>
-      <Textarea />
-      <Button title="Enviar" />
+    <form className={styles.question_form}>
+      <h2>Como posso ajudar?</h2>
+      <div className={styles.form_question_content}>
+        <Textarea />
+        <Button />
+      </div>
     </form>
   );
 }
