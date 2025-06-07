d7c015e9a4418d5127b922ff816babb79255a4d3
Lucas <lucas.almdiniz@gmail.com>
2025-06-07 00:22:40 -0400

    refactor: Refatoraçao do componente Textarea

--- src/components/atoms/Textarea/index.tsx
+++ src/components/atoms/Textarea/index.tsx
@@ -1,7 +1,30 @@
+import { ChangeEventHandler, useEffect, useRef, useState } from 'react';
+import styles from './styles.module.scss';
+
 export default function Textarea() {
+  const [value, setValue] = useState('');
+  const textareaRef = useRef(null);
+
+  const handleChange: ChangeEventHandler<HTMLTextAreaElement>  = ({ target }) => {
+    setValue(target.value);
+  }
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      const textarea = textareaRef.current as HTMLTextAreaElement;
+      textarea.style.height = 'auto';
+      textarea.style.height = `${textarea.scrollHeight}px`
+    }
+  }, [value])
+
   return(
-    <div className="mb-3">
-      <textarea className="form-control" id="question-input" rows={3} placeholder="Como posso ajudar?" />
-    </div>
+    <textarea
+      ref={textareaRef}
+      className={styles.textarea_question}
+      id="question-input"
+      placeholder="Pergunte algo!"
+      onChange={handleChange}
+      value={value}
+    />
   );
 }
