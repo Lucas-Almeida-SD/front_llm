2f5866ee1168a8d3c67ef115111e04085269a16b
Lucas <lucas.almdiniz@gmail.com>
2025-06-07 00:28:04 -0400

    feat: Adicionado Context Provider e os componentes Header e Form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
+import Form from "@/components/molecules/Form";
+import Header from "@/components/molecules/Header";
+import { ProviderLLMquestion } from "@/contexts/LLMquestion";
+
+import styles from '@/styles/Home.module.css';
+
 export default function Home() {
   return (
-    <>
-    </>
+    <ProviderLLMquestion>
+      <div className={styles.home_page}>
+        <Header />
+        <main className={styles.main_content}>
+          <Form />
+        </main>
+      </div>
+    </ProviderLLMquestion>
   );
 }
