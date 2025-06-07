43a39526a640a6210aa76cb4bd01c2e8c055dd44
Lucas <lucas.almdiniz@gmail.com>
2025-06-07 00:19:01 -0400

    feat: Criado context para o LLM Question

---
+++ src/contexts/LLMquestion.tsx
@@ -0,0 +1,33 @@
+import { createContext, Dispatch, ReactElement, SetStateAction, useContext, useState } from "react";
+import { LLMs } from "@/utils/constants";
+
+type LLMquestionContextType = {
+  llmSelected: string,
+  setllmSelected: Dispatch<SetStateAction<string>>,
+  question: string,
+  setQuestion: Dispatch<SetStateAction<string>>,
+  questionSent: boolean,
+  setQuestionSent: Dispatch<SetStateAction<boolean>>,
+}
+
+export const LLMquestionContext = createContext<LLMquestionContextType | undefined>(undefined);
+
+export function ProviderLLMquestion({ children }: { children: ReactElement }) {
+  const [llmSelected, setllmSelected] = useState(LLMs[0]);
+  const [question, setQuestion] = useState('');
+  const [questionSent, setQuestionSent] = useState(false);
+
+  const data = {
+    llmSelected, setllmSelected,
+    question, setQuestion,
+    questionSent, setQuestionSent
+  }
+
+  return(
+    <LLMquestionContext.Provider value={data}>
+      {children}
+    </LLMquestionContext.Provider>
+  )
+}
+
+export const useLLMquestionContext = () => useContext(LLMquestionContext);
