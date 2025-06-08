46e961d81fdec54f4f3584fab5915ac485a6dd5c
Lucas <lucas.almdiniz@gmail.com>
2025-06-08 18:26:41 -0400

    refactor: Alterado componente Select por SelectItem
    Adicionada também mais um componente SelectItem para o "TEAM"

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,13 +1,30 @@
-import Select from "@/components/atoms/Select";
+import SelectItem from "@/components/atoms/SelectItem";
 import { useLLMquestionContext } from "@/contexts/LLMquestion";
-import { LLMs } from "@/utils/constants";
+import { LLMs, TEAMS } from "@/utils/constants";
+
+import styles from './styles.module.scss';
 
 export default function Header() {
   const context = useLLMquestionContext();
 
   return(
-    <header>
-      { context && <Select dataList={LLMs} dispatch={context.setllmSelected}/> }
+    <header className={styles.header}>
+      { context && (
+        <>
+          <SelectItem
+            dataList={LLMs}
+            value={context.llmSelected}
+            label="LLM"
+            dispatch={context.setllmSelected}
+          />
+          <SelectItem
+            dataList={TEAMS}
+            value={context.teamSelected}
+            label="TEAM"
+            dispatch={context.setTeamSelected}
+          />
+        </>
+      )}
     </header>
   );
 }
