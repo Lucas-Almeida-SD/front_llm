363f2bd959ab019261e8be51f38b5628389850f1
Lucas <lucas.almdiniz@gmail.com>
2025-06-08 22:19:29 -0400

    feat: Adicionado componente PageHeader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Form from "@/components/molecules/Form";
 import Header from "@/components/molecules/Header";
 import LoadingText from "@/components/molecules/LoadingText";
+import PageHeader from "@/components/molecules/PageHeader";
 import { ProviderLLMquestion } from "@/contexts/LLMquestion";
 
 import styles from '@/styles/Home.module.scss';
@@ -8,13 +9,16 @@ import styles from '@/styles/Home.module.scss';
 export default function Home() {
   return (
     <ProviderLLMquestion>
-      <div className={styles.home_page}>
-        <Header />
-        <LoadingText />
-        <main className={styles.main_content}>
-          <Form />
-        </main>
-      </div>
+      <>
+        <PageHeader />
+        <div className={styles.home_page}>
+          <Header />
+          <LoadingText />
+          <main className={styles.main_content}>
+            <Form />
+          </main>
+        </div>
+      </>
     </ProviderLLMquestion>
   );
 }
